b8f189d6b6d8c8b7e69ea11ade36f8df32a0a39f
omta <omtadayma2001@gmail.com>
2024-05-18 16:54:50 +0530

    mongodb connection

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ import 'dotenv/config'
 import cors from 'cors'
 import cookieParser from "cookie-parser"
 import fileUpload from "express-fileupload";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB)
+.then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((error)=>{
+    console.log(error);
+})
 
 const app = express();
 app.use(cors({
