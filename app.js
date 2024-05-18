5d5ea6e6f2b8ae27678f4bee4ff3e391190b8fa2
omta <omtadayma2001@gmail.com>
2024-05-18 15:45:38 +0530

    import packages

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 import express from "express"
 import 'dotenv/config'
-
+import cors from 'cors'
+import cookieParser from "cookie-parser"
+import fileUpload from "express-fileupload";
 
 const app = express();
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods:["GET","POST","DELETE","PUT"],
+    credentials: true
+}))
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp',
+}))
 
 export default app;
