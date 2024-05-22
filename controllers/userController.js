ed9658632518c0781931c3f03744e00e397bdc6a
omta <omtadayma2001@gmail.com>
2024-05-22 14:51:48 +0530

    create register route

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import ErrorHandler from "../middlewares/error.js";
+import { User } from "../models/userModel.js";
+
+export const register = catchAsyncError(async(req,res,next)=>{
+    const {name,email,password,phone,role} = req.body;
+
+    if(!name || !email || !password || !phone || !role){
+        return next(new ErrorHandler("Please fill all the fields",400));
+    }
+    const isEmail = await User.findOne({email});
+    if(isEmail){
+        return next(new ErrorHandler("Email already exists",400));
+    }
+    const user = await User.create({name,email,password,phone,role});
+    console.log("User Register SuccessFully",user)
+})
+
