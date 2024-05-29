80f8bab5b7d0cf7858764c1c310b1f1e68aa037e
omta <omtadayma2001@gmail.com>
2024-05-29 17:03:10 +0530

    add update jobs functionality

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -47,3 +47,24 @@ export const getMyJobs = catchAsyncError(async(req,res,next)=>{
         myjobs
     })
 })
+
+export const updateJob = catchAsyncError(async(req,res,next)=>{
+    const {role} = req.user;
+    if(role === "Job Seeker"){
+        return next(new ErrorHandler("Job Seeker not allowed to access this resource",400))
+    };
+    const {id} = req.params;
+    let job = await Job.findById(id);
+    if(!job){
+        return next(new ErrorHandler("Job not found",404));
+    }
+    job = await Job.findByIdAndUpdate(id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    })
+    res.status(200).json({
+        success:true,
+        message:"Job Updated Successfully!",
+    })
+})
--- routes/jobRoute.js
+++ routes/jobRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getAllJobs, getMyJobs, postJob } from '../controllers/jobController.js';
+import { getAllJobs, getMyJobs, postJob, updateJob } from '../controllers/jobController.js';
 import { isAuthenticated } from '../middlewares/auth.js';
 
 const router = express.Router()
 
 router.get("/getall",getAllJobs);
 router.post("/postjob",isAuthenticated,postJob);
-router.get("/getmyjobs",isAuthenticated,getMyJobs)
+router.get("/getmyjobs",isAuthenticated,getMyJobs);
+router.put("/update/:id",isAuthenticated,updateJob);
 
 export default router;
