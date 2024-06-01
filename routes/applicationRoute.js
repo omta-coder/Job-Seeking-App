75cd0ec02c0e114ad6bdac1a508df8ad8960a74d
omta <omtadayma2001@gmail.com>
2024-06-01 20:37:55 +0530

    create application route

--- routes/applicationRoute.js
+++ routes/applicationRoute.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import { isAuthenticated } from '../middlewares/auth.js';
+import { employerGetAllApplications, jobseekerDeleteApplication, jobseekerGetAllApplications } from '../controllers/applicationController.js';
 
-const router = express.Router()
+
+const router = express.Router();
+
+router.get("/employer/getall",isAuthenticated,employerGetAllApplications);
+router.get("/jobseeker/getall",isAuthenticated,jobseekerGetAllApplications);
+router.delete("/delete/:id",isAuthenticated,jobseekerDeleteApplication);
 
 
 export default router;
