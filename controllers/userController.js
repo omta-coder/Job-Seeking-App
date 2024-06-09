8332857dc54139af89efbe9fdbab67637ef79939
omta <omtadayma2001@gmail.com>
2024-06-09 18:41:44 +0530

    create getUser route

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,11 @@ export const logout = catchAsyncError(async(req,res,next)=>{
         message:"Logout Successfully"
     })
 })
+
+export const getUser = catchAsyncError((req, res, next) => {
+    const user = req.user;
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  });
