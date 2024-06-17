5bbf4dd40128d22f92234dcd4ff1fa9f6472a1e8
omta <omtadayma2001@gmail.com>
2024-06-17 13:14:45 +0530

    create AppWrapper

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+export const contet = createContext({
+  isAuthorized:false
+})
+
+const AppWrapper = ()=>{
+  const [isAuthorized, setIsAuthorized] = useState(false)
+  const [user, setUser] = useState({});
+  return (
+    <contet.Provider value={{isAuthorized, setIsAuthorized, user, setUser}}>
+      <App/>
+    </contet.Provider>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
 )
