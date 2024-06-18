797b62812f1bcb1135fee6fa4ea409758a1f3100
omta <omtadayma2001@gmail.com>
2024-06-18 20:14:13 +0530

    add routes

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './App.css'
+import { Context } from './main'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useContext } from 'react';
+import Home from './component/Home/Home';
+import Navbar from './component/Layout/Navbar';
+import Footer from './component/Layout/Footer';
+import Login from './component/Auth/Login';
+import Register from './component/Auth/Register';
+
 
 const App = () => {
+  const {isAuthorized,setIsAuthorized,setUser} = useContext(Context)
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register/>} />
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+    </>
   )
 }
 
