2e6bb3a1541aeda925c920c7b08ca307a9bed4e1
omta <omtadayma2001@gmail.com>
2024-06-19 18:18:47 +0530

    create routes

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -8,6 +8,13 @@ import Navbar from './component/Layout/Navbar';
 import Footer from './component/Layout/Footer';
 import Login from './component/Auth/Login';
 import Register from './component/Auth/Register';
+import NotFound from './component/NotFound/NotFound';
+import MyApplications from './component/Application/MyApplications';
+import PostJob from './component/Job/PostJob';
+import MyJobs from './component/Job/MyJobs';
+import Jobs from './component/Job/Jobs';
+import JobDetails from './component/Job/JobDetails';
+import Application from './component/Application/Application';
 
 
 const App = () => {
@@ -20,6 +27,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register/>} />
+      <Route path="*" element={<NotFound/>} />
     </Routes>
     <Footer/>
     </BrowserRouter>
