67de58c34029df5833c1d9c6f5d912e6b276340c
Omta <omtadayma2001@gmail.com>
2024-08-31 12:13:54 +0530

    Create Home Page

--- FRONTEND/src/component/Home/Home.jsx
+++ FRONTEND/src/component/Home/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../../main'
+import { Navigate } from "react-router-dom";
+import HowItWorks from './HowItWorks';
+import HeroSection from './HeroSection';
 
 const Home = () => {
+  const {isAuthorized} = useContext(Context)
+  if(!isAuthorized){
+    return <Navigate to={"/login"}/>
+  }
   return (
-    <div>Home</div>
+    <>
+     <section className="homePage page">
+      <HeroSection/>
+      <HowItWorks/>
+     </section>
+    </>
   )
 }
 
